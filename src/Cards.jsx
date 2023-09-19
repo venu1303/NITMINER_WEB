ebafc00c79019ddc066ba64a8f5096a0e8d3df54
Venu1303 <venuthegreat2@gmail.com>
2023-09-20 00:12:10 +0530

    updated page

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -5,18 +5,18 @@ import CardItem from './CardItem';
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>Check out these EPIC Services</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src="img-9.jpg"
+              src="images/i1.jpeg"
               text='Explore the hidden waterfall deep inside the Amazon Jungle'
               label='Adventure'
               path='/services'
             />
             <CardItem
-              src='img-2.jpg'
+              src='images/i2.jpeg'
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
               path='/services'
@@ -24,19 +24,19 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
+              src='images/i3.jpeg'
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
+              src='images/i4.jpeg'
               text='Experience Football on Top of the Himilayan Mountains'
               label='Adventure'
               path='/products'
             />
             <CardItem
-              src='images/img-8.jpg'
+              src='images/i5.jpeg'
               text='Ride through the Sahara Desert on a guided camel tour'
               label='Adrenaline'
               path='/sign-up'
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,19 +1,9 @@
+import HeroSection from "./HeroSection";
+
 const Home = () => {
     return (
         <>
-            <div className="home-page">
-                <div className="home-left flex-fix">
-                    <p className="home-left-content">
-                        convert your e-contract to smart contract with <strong className="brand">NITMINER TECHNOLOGIES PRIVATE LIMITED</strong>
-                    </p>
-                    <button className="home-left-btn flex-fix">
-                        Get Started
-                    </button>
-                </div>
-                <div className="home-right flex-fix">
-                    <img src="https://cdn.vector.com/_processed_/7/6/csm_SW_Test_Coins_Line_Right_stage_slider_a83b306d97.jpg" alt="right-home-img" srcset="" />
-                </div>
-            </div>
+           <HeroSection/>
         </>
       );
 }
